0bd2a259058c671915f4e30e0e9c190adc044e1f
Matthias Zirnstein <matthias.zirnstein@googlemail.com>
2011-05-10 00:21:20 -0700

    use correct class

--- lib/controller_concerned.rb
+++ lib/controller_concerned.rb
@@ -1,3 +1,3 @@
 require File.expand_path("../controller_concerned/base", __FILE__)
 
-ApplicationController.send(:include, ControllerConcerned::Base)
+ActionController::Base.send(:include, ControllerConcerned::Base)
